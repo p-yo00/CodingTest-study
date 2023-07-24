d13d2f52b0348175c3a7de77d0e34e06857c76f5
p-yo00 <p-yo00@naver.com>
2023-07-24 23:31:55 +0900

    s기준 정렬한 풀이

---
+++ FreeSpace/Lv2)요격시스템2.py
@@ -0,0 +1,22 @@
+def solution(targets):
+    answer = 0
+    targets.sort()
+    jump = 0
+
+    for x in range(len(targets)):
+        if jump > 0:
+            jump -= 1
+            continue
+        lst = [targets[x]]
+
+        for y in range(x + 1, len(targets)):
+            if targets[y][0] + 0.5 > targets[x][1] - 0.5:
+                break
+            if targets[x][1]>targets[y][1]:
+                x=y
+            lst.append(targets[y])
+
+        answer += 1
+        jump = len(lst) - 1
+
+    return answer
