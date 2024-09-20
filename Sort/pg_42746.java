9e925b9ddec81d45aa761264c7663dc76683d8cf
yeon <p-yo00@naver.com>
2024-09-21 03:26:50 +0900

    프로그래머스 42746 : 가장 큰 수

--- Sort/pg_42746.java
+++ Sort/pg_42746.java
@@ -2,7 +2,52 @@ package org.example.Sort;
 
 public class pg_42746 {
 
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public String findMax(String[] arr, int cur) {
+        List<String>[] radix = new ArrayList[10];
+        StringBuilder result = new StringBuilder();
+
+        String equal = arr[0];
+        int maxLen = 0;
+        boolean equalCheck = false;
+        for (String a : arr) {
+            if (!equal.equals(a)) {
+                equalCheck = true;
+            }
+            maxLen = Math.max(maxLen, a.length());
+        }
+        if (!equalCheck || cur >= (maxLen+maxLen)) { //
+            for (String a : arr) {
+                result.append(a);
+            }
+            return result.toString();
+        }
+
+        for (int i = 0; i < 10; i++) {
+            radix[i] = new ArrayList<>();
+        }
+
+        for (String a : arr) {
+            if (a.length()-1 < cur) {
+                int idx = Integer.parseInt(String.valueOf((a.charAt(cur%a.length()))));
+                radix[idx].add(a);
+            } else {
+                radix[Integer.parseInt(String.valueOf(a.charAt(cur)))].add(a);
+            }
+        }
+        for (int i = 9; i >= 0; i--) {
+            if (radix[i].size() == 1) {
+                result.append(radix[i].get(0));
+            } else if (radix[i].size() > 1) {
+                result.append(findMax(radix[i].toArray(String[]::new), cur+1));
+            }
+        }
+        return result.toString();
+    }
+
+    public String solution(int[] numbers) {
+        String[] arr = Arrays.stream(numbers).mapToObj(String::valueOf).toArray(String[]::new);
+        String answer = findMax(arr, 0);
+
+        return (answer.charAt(0)=='0')?"0":answer;
     }
 }
