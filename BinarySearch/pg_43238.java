5ac4901390babf98c0e4dce1ded891fa201221aa
yeon <p-yo00@naver.com>
2024-09-24 21:06:39 +0900

    프로그래머스 43238 : 입국심사

---
+++ BinarySearch/pg_43238.java
@@ -0,0 +1,32 @@
+package org.example.BinarySearch;
+
+import java.util.Arrays;
+
+public class pg_43238 {
+
+    public static long solution(int n, int[] times) {
+        long answer = 0;
+        long left = 1;
+        long right = times[0];
+        for (int time : times) {
+            right = Math.min(right, time);
+        }
+        right *= n;
+
+        while (left <= right) {
+            long mid = left + (right - left) / 2;
+            long cur = 0;
+            for (int time : times) {
+                cur += mid/time;
+                if (cur > n) break;
+            }
+            if (cur < n) {
+                left = mid + 1;
+            } else if (cur >= n) {
+                answer = mid;
+                right = mid - 1;
+            }
+        }
+        return answer;
+    }
+}
