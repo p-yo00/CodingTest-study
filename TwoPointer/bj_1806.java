df58d7941f3793938cc489f1d0d00e8320955fcc
yeon <p-yo00@naver.com>
2024-09-26 02:13:08 +0900

    백준 1806 : 부분 합

---
+++ TwoPointer/bj_1806.java
@@ -0,0 +1,39 @@
+package org.example.TwoPointer;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
+// 문제 주소: https://www.acmicpc.net/problem/1806
+public class bj_1806 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader("input.txt"));
+        String[] input = br.readLine().split(" ");
+        int N = Integer.parseInt(input[0]);
+        int S = Integer.parseInt(input[1]);
+        int[] arr = Arrays.stream(br.readLine().split(" "))
+            .mapToInt(Integer::parseInt)
+            .toArray();
+
+        int answer = N+1;
+        for (int i = 0; i < N; i++) {
+            int sum = 0;
+            for (int j = i; j < N; j++) {
+                sum += arr[j];
+                if (j-i+1 >= answer) break;
+                if (sum >= S) {
+                    answer = j-i+1;
+                    break;
+                }
+            }
+        }
+
+        if (answer == N+1) {
+            System.out.println(0);
+        } else {
+            System.out.println(answer);
+        }
+    }
+}
